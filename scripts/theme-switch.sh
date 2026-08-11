a0388a6b5497e05a2301962f6f14b3e88d068115
nainteeth <knusperpommes@gmail.com>
2026-08-11 21:45:55 +0200

    comments

--- scripts/theme-switch.sh
+++ scripts/theme-switch.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Usage: theme-switch.sh [dark|light|catppuccin-latte|catppuccin-frappe|catppuccin-macchiato|catppuccin-mocha]
+# This script only switches the gtk theme and sets the color scheme to be either light or dark. This is what some applications use to determine whether to use a light or dark theme, including Discord sometimes. I have not yet figured out if it is dependent on the current moon phase or what. Discord is just being Discord. Please contact me if you for some reason read this AND figure out a solution. Thanks.
 
 set -euo pipefail
 
